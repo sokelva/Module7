f631fa1281601cf134742efb6b18ce8233ab8158
sokelva <sokelva@mail.ru>
2022-12-29 14:59:47 +0300

    Задание 7.2.14

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -304,87 +304,111 @@ namespace ConsoleApp1
     //}
     //=====================================================================
     //ИНДЕКСАТОРЫ
-    class Book
-    {
-        public string Name;
-        public string Author;
-    }
+    //class Book
+    //{
+    //    public string Name;
+    //    public string Author;
+    //}
 
-    class BookCollection
-    {
-        private Book[] collection;
+    //class BookCollection
+    //{
+    //    private Book[] collection;
 
-        public BookCollection(Book[] collection)
+    //    public BookCollection(Book[] collection)
+    //    {
+    //        this.collection = collection; // this указывает на private Book[] collection;
+
+    //    }
+
+    //    // Индексатор по массиву
+    //    public Book this[int index]
+    //    {
+    //        get
+    //        {
+    //            // Проверяем, чтобы индекс был в диапазоне для массива
+    //            if (index >= 0 && index < collection.Length)
+    //            {
+    //                return collection[index];
+    //            }
+    //            else
+    //            {
+    //                return null;
+    //            }
+    //        }
+
+    //        private set
+    //        {
+    //            // Проверяем, чтобы индекс был в диапазоне для массива
+    //            if (index >= 0 && index < collection.Length)
+    //            {
+    //                collection[index] = value;
+    //            }
+    //        }
+    //    }
+
+    //    public Book this[string name]
+    //    {
+    //        get
+    //        {
+    //            for (int i = 0; i < collection.Length; i++)
+    //            {
+    //                if (collection[i].Name == name)
+    //                {
+    //                    return collection[i];
+    //                }
+    //            }
+
+    //            return null;
+    //        }
+    //    }
+    //}
+
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+
+    //        var array = new Book[] {
+    //        new Book { Name = "Мастер и Маргарита", Author = "М.А. Булгаков" },
+    //        new Book { Name = "Отцы и дети", Author = "И.С. Тургенев" },
+    //    };
+    //        BookCollection collection = new BookCollection(array);
+
+    //        Console.ReadKey();
+
+    //        Book book = collection[1];
+    //        book = collection[-1];
+    //        book = collection[4];
+
+    //        Console.ReadKey();
+
+    //        book = collection["Мастер и Маргарита"];
+
+    //        Console.ReadKey();
+    //    }
+    //}
+
+    class IndexingClass
+    {
+        private int[] array;
+
+        public IndexingClass(int[] array)
         {
-            this.collection = collection; // this указывает на private Book[] collection;
-
+            this.array = array;
         }
 
-        // Индексатор по массиву
-        public Book this[int index]
+        //ИНДЕКСАТОР ПО МАССИВУ
+        public int this[int index]
         {
             get
             {
-                // Проверяем, чтобы индекс был в диапазоне для массива
-                if (index >= 0 && index < collection.Length)
-                {
-                    return collection[index];
-                }
-                else
-                {
-                    return null;
-                }
+                return array[index];
             }
-
-            private set
+            set
             {
-                // Проверяем, чтобы индекс был в диапазоне для массива
-                if (index >= 0 && index < collection.Length)
-                {
-                    collection[index] = value;
-                }
+                array[index] = value;
             }
         }
 
-        public Book this[string name]
-        {
-            get
-            {
-                for (int i = 0; i < collection.Length; i++)
-                {
-                    if (collection[i].Name == name)
-                    {
-                        return collection[i];
-                    }
-                }
-
-                return null;
-            }
-        }
-    }
-
-    class Program
-    {
-        static void Main(string[] args)
-        {
-
-            var array = new Book[] {
-            new Book { Name = "Мастер и Маргарита", Author = "М.А. Булгаков" },
-            new Book { Name = "Отцы и дети", Author = "И.С. Тургенев" },
-        };
-            BookCollection collection = new BookCollection(array);
-
-            Console.ReadKey();
-
-            Book book = collection[1];
-            book = collection[-1];
-            book = collection[4];
-
-            Console.ReadKey();
-
-            book = collection["Мастер и Маргарита"];
-
-            Console.ReadKey();
-        }
     }
 }
