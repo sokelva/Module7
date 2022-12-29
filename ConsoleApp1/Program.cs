afbaee63abc62255f683ccf27e6117747d466db4
sokelva <sokelva@mail.ru>
2022-12-29 09:51:22 +0300

    Переопределение свойств

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -113,4 +113,64 @@ namespace ConsoleApp1
         }
     }
 
+    class Citizen
+    {
+        public virtual double Age
+        {
+            get;
+            set;
+        }
+
+        public void SayYourAge()
+        {
+            Console.WriteLine("Мне {0} лет", Age);
+        }
+    }
+
+    class CivilServant : Citizen
+    {
+        private double age;
+        public override double Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value < 18)
+                {
+                    Console.WriteLine("Для работы госслужащим гражданин должен быть не младше 18 лет");
+                }
+                else
+                {
+                    age = value;
+                }
+            }
+        }
+    }
+
+    class President : CivilServant
+    {
+        private double age;
+        public override double Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value < 35)
+                {
+                    Console.WriteLine("Для работы президентом гражданин должен быть не младше 35 лет");
+                }
+                else
+                {
+                    age = value;
+                }
+            }
+        }
+    }
+
 }
